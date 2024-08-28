75c6d63331867f0d68d942d0e222fb3cb4fad962
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2024-08-28 11:16:59 +0200

    removed docker, add github action workflow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta
-from FlightRadar24 import FlightRadar24API
 import json
 import os
-import requests  # Import the requests library
+import requests
+from datetime import datetime, timedelta
+from FlightRadar24 import FlightRadar24API
 
 # Initialize API
 fr_api = FlightRadar24API()
@@ -18,6 +18,34 @@ cooldown_period = timedelta(minutes=60)  # 1 hour cooldown period
 state_file = 'runway_state.json'
 notification_url = "https://ntfy.sh/uilenstede102_flight"  # Ensure the URL is valid
 
+Notifications = {
+    "active": {
+        "headers": {
+            "Title": "ACTIVE RUNWAY",
+            "Priority": "urgent",
+            "Tags": "flight_departure,skull",
+            "Actions": [
+                {
+                    "action": "view",
+                    "label": "Flight Tracker",
+                    "url": "https://flightradar24.com"
+                }
+            ]
+        },
+        "message": "Put on ANC"
+    },
+    "inactive": {
+        "headers": {
+            "Title": "Inactive Runway",
+            "Priority": "urgent",
+            "Tags": "white_check_mark,shushing_face"
+        },
+
+        "message": "Enjoy your peace"
+    }
+}
+
+
 # Function to check if a flight is aligned with the runway
 def is_flight_heading_to_runway(heading):
     for runway in runway_heading:
@@ -30,7 +58,9 @@ def load_state():
     if os.path.exists(state_file):
         with open(state_file, 'r') as f:
             return json.load(f)
-    return {'last_active': None, 'runway_active': False}
+    else:
+        # Create a default state if the file doesn't exist
+        return {'last_active': None, 'runway_active': False}
 
 # Function to save state
 def save_state(state):
@@ -38,9 +68,10 @@ def save_state(state):
         json.dump(state, f)
 
 # Function to send notification
-def send_notification(message):
+def send_notification(notification_content):
     try:
-        response = requests.post(notification_url, data=message)
+        response = requests.post(notification_url, data=notification_content['message'], headers=notification_content['headers'])
+        print(notification_content['headers']['Title'])
         response.raise_for_status()  # Raises an HTTPError if the response was an error
     except requests.RequestException as e:
         print(f"Failed to send notification: {e}")
@@ -66,7 +97,7 @@ current_time = datetime.utcnow()
 if relevant_flights:
     if not runway_active or (last_active_time and current_time - last_active_time > cooldown_period):
         # Send notification
-        send_notification("A flight is heading towards the runway.")
+        send_notification(Notifications['active'])
         # Update state
         state['runway_active'] = True
         state['last_active'] = current_time.strftime('%Y-%m-%d %H:%M:%S')
@@ -74,7 +105,7 @@ if relevant_flights:
 else:
     if runway_active and (last_active_time and current_time - last_active_time > timedelta(minutes=5)):
         # Send notification that runway is no longer active
-        send_notification("The runway is now inactive.")
+        send_notification(Notifications['inactive'])
         # Update state
         state['runway_active'] = False
         state['last_active'] = None
