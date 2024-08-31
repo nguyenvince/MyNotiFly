7e8b71e3b5d67c03b4174a27b04bdeb15dd5e205
Vince Nguyen <55157813+nguyenvince@users.noreply.github.com>
2024-08-31 17:54:59 +0200

    Revert "removed json, saved data to env var"
    This reverts commit 65f46cdb7aadc5d9afdb88e1b6989d2bafbebf90.

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import urllib.request
 from datetime import datetime, timedelta
 from FlightRadar24 import FlightRadar24API
 
-env_file = os.getenv('GITHUB_ENV')
-print(env_file)
-
 arrival = {
     'name': 'Arrival',
     'lat': float(os.environ['ARRIVAL_LAT']),
@@ -21,7 +18,7 @@ departure = {
     'heading': int(os.environ['DEPARTURE_HEADING']),
 }
 
-noti_url = os.environ['NOTI_URL']
+noti_url = os.getenv('NOTI_URL')
 
 radius = 5000  # meters
 maxHeight = 3000  # feet
@@ -29,6 +26,7 @@ maxSpeed = 300  # knots
 minSpeed = 100
 heading_tolerance = 10  # degrees tolerance for runway alignment
 cooldown_period = timedelta(minutes=10)  # 10 min cooldown period to determine if the last flight has already passed
+state_file = 'runway_state.json'
 
 THRESHOLD_CONSECUTIVE_FLIGHTS = 3
 
@@ -53,29 +51,24 @@ Notifications = {
 }
 
 
+
 # Function to check if a flight is aligned with the runway
 def is_flight_heading_to_runway(heading, runway_heading):
     return abs(heading - runway_heading) <= heading_tolerance
 
-# Function to load state from environment variables
+# Function to load state
 def load_state():
-    last_active = os.getenv('LAST_ACTIVE')
-    consecutive_flights = int(os.getenv('CONSECUTIVE_FLIGHTS', 0))
+    if os.path.exists(state_file):
+        with open(state_file, 'r') as f:
+            return json.load(f)
+    else:
+        # Create a default state if the file doesn't exist
+        return {'last_active': None, 'consecutive_flights': 0}
 
-    state = {
-        'last_active': last_active,
-        'consecutive_flights': consecutive_flights
-    }
-    print(state)
-
-    return state
-
-# Function to save state to environment variables
+# Function to save state
 def save_state(state):
-    with open(env_file, "a") as env_file:
-        env_file.write(f"LAST_ACTIVE={state['last_active'] if state['last_active'] else ""}")
-        env_file.write(f"\n")
-        env_file.write(f"CONSECUTIVE_FLIGHTS={state['consecutive_flights']}")
+    with open(state_file, 'w') as f:
+        json.dump(state, f)
 
 # Function to send notification
 def send_notification(notification_content):
@@ -151,14 +144,13 @@ if __name__ == '__main__':
                 print(f"Notification not sent, runway has been ACTIVE for a long time. Consecutive count: {consecutive_flights}")
 
             save_state(state)
-            break  # Stop checking after sending a notification
+            break  # Stop checking 
         else:
             if last_active_time and current_time - last_active_time > cooldown_period:
                 if consecutive_flights >= THRESHOLD_CONSECUTIVE_FLIGHTS:
                     # Send inactive notification if enough flights have passed with no new ones detected
                     send_notification(Notifications['inactive'])  
                     notification_sent = True  # Set flag to indicate a notification was sent
-                    
                 else:
                     print(f"Fewer than {THRESHOLD_CONSECUTIVE_FLIGHTS} consecutive flights. No inactive notification sent.")
                 
@@ -166,7 +158,7 @@ if __name__ == '__main__':
                 state['last_active'] = None
                 state['consecutive_flights'] = 0
                 save_state(state)
-
+                
                 break  # Stop checking 
             else:
                 print("No recent flights, runway remains inactive.")
